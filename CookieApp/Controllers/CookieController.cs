a4104505d0fa3dc118e2b634a8a20d0590bac1e9
Samuel <padmadansamuel@gmail.com>
2024-09-14 20:31:10 +0530

    Add project files.

---
+++ CookieApp/Controllers/CookieController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieApp.Controllers
+{
+    public class CookieController : Controller
+    {
+        // GET: Cookie
+
+        public ActionResult SetCookie()
+        {
+            HttpCookie cookie = new HttpCookie("UserName");
+            cookie.Value = "Samuel";
+            cookie.Expires = DateTime.Now.AddDays(7);
+            Response.Cookies.Add(cookie);
+
+            return View(); 
+        }
+
+    
+        public ActionResult GetCookie()
+        {
+            HttpCookie cookie = Request.Cookies["UserName"];
+            if (cookie != null)
+            {
+                ViewBag.CookieValue = cookie.Value;
+            }
+            else
+            {
+                ViewBag.CookieValue = null;
+            }
+
+            return View();
+        }
+
+      
+        public ActionResult DeleteCookie()
+        {
+            HttpCookie cookie = new HttpCookie("UserName");
+            cookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(cookie);
+
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View(); 
+        }
+    }
+}
